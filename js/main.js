c3af5e008eeb16d3c09eeb053105dd6f81080d1a
Horbach Nikola <horbach.mykola@airslate.com>
2021-06-20 20:10:52 +0300

    lint fixes

--- js/main.js
+++ js/main.js
@@ -1,40 +1,33 @@
 // Рандомное значение
-let getRandomize = function (minValue, maxValue) {
-  let randomize = minValue + Math.random() * (maxValue + 1 - minValue);
+const getRandomize = function (minValue, maxValue) {
+  const randomize = minValue + Math.random() * (maxValue + 1 - minValue);
 
   if (minValue >=0 && maxValue >= 0) {
     switch (minValue >=0 && maxValue >= 0) {
 
       case minValue > maxValue:
-        console.log('Max value must be greater than min value');
-        break
+        return 'Max value must be greater than min value';
 
       case minValue === maxValue:
-        console.log('Min value cannot be equal to the max value')
-        break
+        return 'Min value cannot be equal to the max value';
 
       default:
         return Math.floor(randomize);
-    };
-  };
+    }
+  }
 
   if (minValue < 0 || maxValue < 0) {
-    console.log('Values can be from 0 and higher')
-  };
+    return 'Values can be from 0 and higher';
+  }
 };
 
 getRandomize(1, 4);
 
 // Проверка строки
-let stringToCheck
-let maxStringLength = 120;
-let getStringCheck = function (stringToCheck, maxStringLength) {
-  if (stringToCheck.length <= maxStringLength) {
-    return true;
-  }
-  return false;
+const stringToCheck = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua';
+const maxStringLength = 120;
+const getStringCheck = function () {
+  return stringToCheck.length <= maxStringLength;
 };
-
-stringToCheck = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
 
 getStringCheck(stringToCheck, maxStringLength);
