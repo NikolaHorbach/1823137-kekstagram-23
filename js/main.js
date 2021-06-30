9b4cab629ba86c626d26b9cbf0bcf09b52ca7231
Horbach Nikola <horbach.mykola@airslate.com>
2021-06-30 11:56:49 +0300

    fixed getRandomize, deleted const from getStringCheck

--- js/main.js
+++ js/main.js
@@ -3,31 +3,20 @@ const getRandomize = function (minValue, maxValue) {
   const randomize = minValue + Math.random() * (maxValue + 1 - minValue);
 
   if (minValue >=0 && maxValue >= 0) {
-    switch (minValue >=0 && maxValue >= 0) {
-
-      case minValue > maxValue:
-        return 'Max value must be greater than min value';
-
-      case minValue === maxValue:
-        return 'Min value cannot be equal to the max value';
-
-      default:
-        return Math.floor(randomize);
+    if (minValue > maxValue) {
+      return 'Max value must be greater than min value';
+    } else if (minValue === maxValue) {
+      return 'Min value cannot be equal to the max value';
     }
+    return Math.floor(randomize);
   }
-
-  if (minValue < 0 || maxValue < 0) {
-    return 'Values can be from 0 and higher';
-  }
+  return 'Values can be from 0 and higher';
 };
 
 getRandomize(1, 4);
 
-// Проверка строки
-const stringToCheck = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua';
-const maxStringLength = 120;
-const getStringCheck = function () {
+const getStringCheck = function (stringToCheck, maxStringLength) {
   return stringToCheck.length <= maxStringLength;
 };
 
-getStringCheck(stringToCheck, maxStringLength);
+getStringCheck('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 120);
